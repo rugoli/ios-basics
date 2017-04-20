46fb483ace94c5c06efb70407242be6d0582da9a
Roshan <roshan@Roshans-Mac-mini.local>
2017-04-19 21:16:09 -0700

    adding brokerage project

---
+++ RGBrokerage/RGBrokerage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RGBrokerage
+//
+//  Created by Roshan Goli on 4/19/17.
+//  Copyright © 2017 Roshan Goli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RGBrokerage/RGBrokerage/RGMainCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  RGMainCollectionView.h
+//  RGBrokerage
+//
+//  Created by Roshan Goli on 4/19/17.
+//  Copyright © 2017 Roshan Goli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGMainCollectionView : UICollectionView
+
+@end

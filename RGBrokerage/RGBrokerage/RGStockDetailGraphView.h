1fd0e5cb035b846cddc2501afc205089a8e21c9a
Roshan <none@none>
2017-06-08 21:11:44 -0700

    adding graph view to detail

---
+++ RGBrokerage/RGBrokerage/RGStockDetailGraphView.h
@@ -0,0 +1,13 @@
+//
+//  RGStockDetailGraphView.h
+//  RGBrokerage
+//
+//  Created by Roshan Goli on 6/8/17.
+//  Copyright © 2017 Roshan Goli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGStockDetailGraphView : UIView
+
+@end
---
+++ RGBrokerage/RGBrokerage/RGStockDetailGraphView.m
@@ -0,0 +1,21 @@
+//
+//  RGStockDetailGraphView.m
+//  RGBrokerage
+//
+//  Created by Roshan Goli on 6/8/17.
+//  Copyright © 2017 Roshan Goli. All rights reserved.
+//
+
+#import "RGStockDetailGraphView.h"
+
+@implementation RGStockDetailGraphView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
